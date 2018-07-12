26330ba0b971b9509bb07c87eba40f9818fa59fd
Sammy Quann <sammyquann@gmail.com>
2018-07-12 16:57:05 -0700

    implemented add button.

--- app/src/main/java/samstequa/com/fridge_recipes/RecipeActivity.java
+++ app/src/main/java/samstequa/com/fridge_recipes/RecipeActivity.java
@@ -4,7 +4,11 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class RecipeActivity extends AppCompatActivity {
@@ -37,8 +41,18 @@ public class RecipeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_recipe);
 
         mTextMessage = (TextView) findViewById(R.id.recipe_header);
+        final EditText search_bar = findViewById(R.id.recipe_search_bar);
+        Button add_btn = findViewById(R.id.recipe_add_ingredient);
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        add_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("=============Ingredient", search_bar.getText().toString());
+            }
+        });
     }
 
 }
